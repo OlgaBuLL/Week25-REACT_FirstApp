2cb293e6d8141ebfcec48a36233c5bee336d5a31
OlgaBuLL <108359930+OlgaBuLL@users.noreply.github.com>
2023-01-26 21:04:56 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import { SwatchesPicker } from "react-color";
+import { SketchPicker } from "react-color";
 
 function App() {
+  const [background, setBackgroundColor] = useState("#BD10E0");
+  const [color, setColor] = useState("#FFFFFF");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <SwatchesPicker
+        color={color}
+        onChangeComplete={(color) => {
+          setColor(color.hex);
+        }}
+      />
+      <SketchPicker
+        color={background}
+        onChangeComplete={(color) => {
+          setBackgroundColor(color.hex);
+        }}
+      />
+      <div
+        style={{
+          background: background,
+          height: "300px",
+          transition: "ease all 500ms",
+          padding: "2rem",
+        }}
+      >
+        <h1 style={{ color: color }}>HELLO WORLD!</h1>
+        <p>This is my 1st REACT App !</p>
+        <p>So exciting!</p>
+        <br></br>
+        <p style={{ fontSize: "small", color: "#C9C4CA" }}>
+          Here U can change the color of the Title and the background
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>Let's try😉</p>
+      </div>
     </div>
   );
 }
